f9cb4c12d6631f71dbe86cc5b7b59aa1e8050c9f
Vitor Afonso <vittokm@hotmail.com>
2023-05-10 12:22:16 +0100

    Create models

---
+++ models/User.model.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required.'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required.'],
+    },
+    incomes: [{ type: Schema.Types.ObjectId, ref: 'Income' }],
+    expenses: [{ type: Schema.Types.ObjectId, ref: 'Expense' }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model('User', userSchema);
